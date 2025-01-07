12fb3b9c0ee1a05822949f766c21916d606d7a6e
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2025-01-07 09:49:03 +0500

    Update threedvis.py

--- threedvis/threedvis.py
+++ threedvis/threedvis.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
-class Viz3D:
+class threedvis:
     """
     A library for easy 3D visualizations using Matplotlib.
     """
